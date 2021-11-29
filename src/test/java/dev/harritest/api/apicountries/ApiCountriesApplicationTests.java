7abcb7efc2eee932aa5568432f3d93a4de057a56
fmohammad <fmohammad@gmail.com>
2021-11-29 19:20:01 +0200

    My first commit

---
+++ src/test/java/dev/harritest/api/apicountries/ApiCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.harritest.api.apicountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
