c1946026a6fa6025cf70ca20d6d36870b1ab9f3f
fmohammad <fmohammad@gmail.com>
2021-11-29 19:46:39 +0200

    add

--- src/main/java/dev/harritest/api/apicountries/TestIfWorking.java
+++ src/main/java/dev/harritest/api/apicountries/TestIfWorking.java
@@ -1,13 +1,13 @@
 package dev.harritest.api.apicountries;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestIfWorking {
 
-	@RequestMapping(method = RequestMethod.GET,path="/say-hi")
+	
+	@GetMapping("/say-hi")
 	public String sayHi() {
 		return "hi iam working <br> lets get started";
 	
