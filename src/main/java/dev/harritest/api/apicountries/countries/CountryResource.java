f145efaa5be0f2017bca4b0837673a36bd3e780f
fmohammad <fmohammad@gmail.com>
2021-11-29 21:48:21 +0200

    add

--- src/main/java/dev/harritest/api/apicountries/countries/CountryResource.java
+++ src/main/java/dev/harritest/api/apicountries/countries/CountryResource.java
@@ -1,11 +1,18 @@
 package dev.harritest.api.apicountries.countries;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CountryResource {
@@ -28,6 +35,20 @@ public class CountryResource {
 		
 		
 		return country;
+	}
+	
+	@PostMapping("/country")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody Country country) {
+		Country savedCountry = service.save(country);
+		// CREATED
+
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{code}")
+			.buildAndExpand(savedCountry.getCode()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 
 
