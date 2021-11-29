db7ae37e78dcac62c9fce3ade9973d0ae864deda
fmohammad <fmohammad@gmail.com>
2021-11-29 22:33:19 +0200

    add

--- src/main/java/dev/harritest/api/apicountries/countries/Country.java
+++ src/main/java/dev/harritest/api/apicountries/countries/Country.java
@@ -12,19 +12,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Country {
-/*
- CREATE TABLE IF NOT EXISTS `countries` (
-  `code` CHAR(2) NOT NULL COMMENT 'Two-letter country code (ISO 3166-1 alpha-2)',
-  `name` VARCHAR(255) NOT NULL COMMENT 'English country name',
-  `full_name` VARCHAR(255) NOT NULL COMMENT 'Full English country name',
-  `iso3` CHAR(3) NOT NULL COMMENT 'Three-letter country code (ISO 3166-1 alpha-3)',
-  `number` SMALLINT(3) ZEROFILL NOT NULL COMMENT 'Three-digit country number (ISO 3166-1 numeric)',
-  `continent_code` CHAR(2) NOT NULL,
-  PRIMARY KEY (`code`),
-  KEY `continent_code` (`continent_code`),
-  CONSTRAINT `fk_countries_continents` FOREIGN KEY (`continent_code`) REFERENCES `continents` (`code`)
-) ENG
- */
 
 	@Id
 	@Size(min=2, message="Name should have atleast 2 characters")
