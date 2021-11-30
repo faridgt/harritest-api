5f3f37de94d5ca019337f38376feaab798d9f495
fmohammad <fmohammad@gmail.com>
2021-11-30 12:18:01 +0200

    add

--- src/main/java/dev/harritest/api/apicountries/countries/CountryDaoService.java
+++ src/main/java/dev/harritest/api/apicountries/countries/CountryDaoService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 
-@Component
+//@Component
 public class CountryDaoService {
 	static private List<Country> countries=new ArrayList<>();
 			static {
