638ab553cffe8425665c7aaedf141e4d61058d1d
fmohammad <fmohammad@gmail.com>
2021-12-01 17:44:26 +0200

    add

--- src/main/java/dev/harritest/api/apicountries/countries/CountryJPAResource.java
+++ src/main/java/dev/harritest/api/apicountries/countries/CountryJPAResource.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +23,15 @@ public class CountryJPAResource {
 	private CountryRepository cntRepository;
 	
 
-	@GetMapping("/countries")
-    public List<Country> retrieveAllContinents(
+	@GetMapping("/countries/{fetchtype}")
+    public List<Country> retrieveAllContinents(@PathVariable String fetchtype,
                         @RequestParam(defaultValue = "0") Integer pageNo, 
                         @RequestParam(defaultValue = "10") Integer pageSize,
                         @RequestParam(defaultValue = "id") String sortBy) 
     {
 		
+		if(fetchtype!=null&&fetchtype.equals("all"))
+			pageSize=10000;
 		
 		Pageable paging = PageRequest.of(pageNo, pageSize);
 		 
